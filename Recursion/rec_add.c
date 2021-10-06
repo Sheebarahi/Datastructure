54e9f81252223cd23b75a2768afe958ff50c31f9
Sheeba rahi <90591767+Sheebarahi@users.noreply.github.com>
2021-10-05 21:59:55 -0700

    Create rec_add.c

---
+++ Recursion/rec_add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <assert.h>
+
+// Rcursion Function 
+
+
+
+//  Sum Of Array
+int array_sum(int arr[], int begin, int end){
+  if (begin >= end) return 0;
+  else return arr[begin] + array_sum(arr, begin + 1, end);
+}
+
+// Multiplication of array
+
+int find_mul(int arr[], int begin,  int size){
+  if (size == 0) return 1;
+  return arr[begin] * find_mul(arr, begin + 1, size - 1);
+  }
+
+// ..............................MAIN FUNCTION......................
+
+
+int main(){
+  int arr[] = {2,3,4,5,6};
+  int size = sizeof arr/ sizeof arr[0];
+  assert(array_sum(arr,0,size) == 20);
+  assert(find_mul(arr,0,size) == 720);
+}
